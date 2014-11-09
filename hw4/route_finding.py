f564d4237b82fa224368dab3d64039c1de44d7ae
teststaybaka <teststaybaka@gmail.com>
2014-11-09 13:16:57 -0500

    read complete

--- hw4/route_finding.py
+++ hw4/route_finding.py
@@ -8,7 +8,15 @@ goal = [float(nums[0]), float(nums[1])]
 f.close()
 
 f = open('hw4_world_and_obstacles_convex.txt', 'r')
-num_obstacles = f.readline()
-print num_obstacles
-
+num_obstacles = int(f.readline())
+data = []
+for i in range(0, num_obstacles):
+	num_vertices = int(f.readline())
+	vertices = []
+	for j in range(0, num_vertices):
+		coord = f.readline().split()
+		x = float(coord[0])
+		y = float(coord[1])
+		vertices.append([x, y])
+	data.append(vertices)
 f.close()
